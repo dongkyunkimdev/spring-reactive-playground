9b19cf242d9274ed78d26b0ea5b8eecd48ab0727
dongkyunkim <ddkds66@gmail.com>
2024-08-24 23:37:48 +0900

    backpressure example

---
+++ src/main/java/com/reactive/playground/demo/BackpressureStrategyBufferDropLatest.java
@@ -0,0 +1,29 @@
+package com.reactive.playground.demo;
+
+import com.reactive.playground.demo.utils.Logger;
+import com.reactive.playground.demo.utils.TimeUtils;
+import reactor.core.publisher.BufferOverflowStrategy;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.Duration;
+
+public class BackpressureStrategyBufferDropLatest {
+    public static void main(String[] args) {
+        Flux
+                .interval(Duration.ofMillis(300L))
+                .doOnNext(data -> Logger.info("# emitted by original Flux: {}", data))
+                .onBackpressureBuffer(2,
+                        dropped -> Logger.info("# Overflow & dropped: {}", dropped),
+                        BufferOverflowStrategy.DROP_LATEST)
+                .doOnNext(data -> Logger.info("# emitted by Buffer: {}", data))
+                .publishOn(Schedulers.parallel(), false, 1)
+                .subscribe(data -> {
+                            TimeUtils.sleep(1000L);
+                            Logger.onNext(data);
+                        },
+                        error -> Logger.onError(error));
+
+        TimeUtils.sleep(3000L);
+    }
+}
---
+++ src/main/java/com/reactive/playground/demo/BackpressureStrategyLatest.java
@@ -0,0 +1,24 @@
+package com.reactive.playground.demo;
+
+import com.reactive.playground.demo.utils.Logger;
+import com.reactive.playground.demo.utils.TimeUtils;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.Duration;
+
+public class BackpressureStrategyLatest {
+    public static void main(String[] args) {
+        Flux
+                .interval(Duration.ofMillis(1L))
+                .onBackpressureLatest()
+                .publishOn(Schedulers.parallel())
+                .subscribe(data -> {
+                            TimeUtils.sleep(5L);
+                            Logger.onNext(data);
+                        },
+                        error -> Logger.onError(error));
+
+        TimeUtils.sleep(2000L);
+    }
+}
