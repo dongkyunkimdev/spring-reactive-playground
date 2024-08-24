a3cf5b34da1283ac327d4b32a70e14a7033beb85
dongkyunkim <ddkds66@gmail.com>
2024-08-24 23:01:46 +0900

    sequence example

---
+++ src/main/java/com/reactive/playground/demo/utils/TimeUtils.java
@@ -0,0 +1,11 @@
+package com.reactive.playground.demo.utils;
+
+public class TimeUtils {
+    public static void sleep(long milliseconds){
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
